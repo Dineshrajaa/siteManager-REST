527512d1bfc1a25464a2971f366c4bef757924fc
Dinesh Raja <dineshrajaa.93@gmail.com>
2017-12-07 23:13:34 +0530

    Included MongoDB URL

--- api/config.js
+++ api/config.js
@@ -1,6 +1,7 @@
 module.exports={
     development:{
         secret: "1a2941d74cfe49979db309998bdcc37f",
-        database: "mongodb://localhost:27017/sitemanager"
+        // database: "mongodb://localhost:27017/sitemanager"
+        database:"mongodb://dinesh:rajaguru@ds133856.mlab.com:33856/sitemanager"
     }
 }
