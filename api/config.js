87f0e9a246fd538beb508dc6a36146a43ad75b25
Dinesh Raja <dineshrajaa.93@gmail.com>
2017-12-09 11:26:03 +0530

    Changed production mode

--- api/config.js
+++ api/config.js
@@ -3,5 +3,9 @@ module.exports = {
         secret: "1a2941d74cfe49979db309998bdcc37f",
         // database: "mongodb://localhost:27017/sitemanager"
         database: "mongodb://dinesh:rajaguru@ds133856.mlab.com:33856/sitemanager"
+    },
+    production: {
+        secret: "1a2941d74cfe49979db309998bdcc37f",
+        database: "mongodb://dinesh:rajaguru@ds133856.mlab.com:33856/sitemanager"
     }
 }
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express'),
-    config = require('./api/config'),
+    config = require('./api/config')[process.env.NODE_ENV || "development"],
     app = express(),
     mongoose = require("mongoose"),
     bodyParser = require("body-parser"),
     port = process.env.PORT || 3000,
     restRoutes = require("./api/controllers");
 mongoose.Promise = global.Promise;
-mongoose.connect(config.development.database);
+mongoose.connect(config.database);
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/api/v1/", restRoutes);
